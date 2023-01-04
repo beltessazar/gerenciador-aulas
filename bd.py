ee7f749f57bd7ad02d10975782639892e5190cb6
Daniel Moraes <dsm0ra3s@gmail.com>
2023-01-04 19:27:53 -0300

    ajustes da tabela

--- bd.py
+++ bd.py
@@ -1,18 +1,30 @@
 import mysql.connector
 import sqlite3
+import datetime
+try:
+        conn = sqlite3.connect('gerenciador_aulas.bd')
+except Error as e:
+        print(e)
 
-conn = sqlite3.connect('gerenciador_aulas')
 cursor = conn.cursor()
+print(conn)
+print(cursor)
 
-def cadastrar_instrumento(nome):
-    return tuple(cursor.execute("""
-    INSERT INTO instrumento VALUES (0, nome)
-    """))
-##def registrar_aula():
+## inserts
+def cadastrar_instrumento(x):
+        cursor.execute("INSERT INTO instrumento (nome) VALUES (?)", (x,))
+        conn.commit()
+def registrar_aula(nome, instrumento, descricao, data):
+        cursor.execute("INSERT INTO registro_aula(nome_aluno, instrumento, descricao_aula, data) VALUES (?, ?, ?, ?)", (nome, instrumento, descricao, data,))
+        conn.commit()
+def cadastrar_aluno(nome, data):
+        cursor.execute("INSERT INTO aluno (nome, data_registro) VALUES (?, ?)", (nome, data))
+        conn.commit()
+## selects
 def buscar_alunos():
     return tuple(cursor.execute("""
     SELECT * FROM aluno;
-    """))    
+    """))
 def buscar_instrumentos():
     return tuple(cursor.execute("""
     SELECT * FROM instrumento;
@@ -29,7 +41,6 @@ def nomes_instrumentos():
     for i in range(len(x)):
         lista.append(x[i][0])
     return lista
-    
 def nomes_alunos():
     lista = []
     x  = tuple(cursor.execute("""
@@ -38,13 +49,15 @@ def nomes_alunos():
     for i in range(len(x)):
         lista.append(x[i][0])
     return lista
-print(type(nomes_instrumentos()))
-print(nomes_instrumentos())
-print(nomes_alunos())
-y = buscar_alunos()
-x = buscar_instrumentos()
-z = buscar_registro_aulas()
-
+##print(type(nomes_instrumentos()))
+##print(nomes_instrumentos())
+##print(nomes_alunos())
+##cadastrar_instrumento('Cavaco')
+##y = buscar_alunos()
+##registrar_aula("João", "Cavaquinho", "Ler livro 1", '05-05-1990')
+##print(buscar_instrumentos())
+##z = buscar_registro_aulas()
+##print(buscar_registro_aulas())
 
 #conn.close()
 ##try:
@@ -57,7 +70,7 @@ z = buscar_registro_aulas()
 ##
 ##    mycursor = mydb.cursor()
 ##
-##    def inserir(tipo_servico, valor_servico, data_servico):   
+##    def inserir(tipo_servico, valor_servico, data_servico):
 ##        sql_insert = """INSERT INTO servicos (id, tipo_servico, valor, data)
 ##                        VALUES (%s,%s,%s,%s)"""
 ##        val = (0, tipo_servico, valor_servico, data_servico)
@@ -67,7 +80,7 @@ z = buscar_registro_aulas()
 ##        mycursor.execute("SELECT * FROM servicos")
 ##        resultado = mycursor.fetchall()
 ##        return resultado
-##            
+##
 ##    mydb.commit()
 ##
 ##    # print(inserir(1, 666, "2020-05-05"))
@@ -76,3 +89,6 @@ z = buscar_registro_aulas()
 ##    # print("Executado antes")
 ##except:
 ##    print("SERVIDOR DESLIGADO! :(")
+x = datetime.datetime.now().strftime("%d/%m/%Y")
+print(x)
+print(buscar_alunos())
--- main.py
+++ main.py
@@ -1,13 +1,94 @@
 from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox
 import sys
 from datetime import datetime
 import bd
 from PyQt5.QtCore import *
 from datetime import datetime
 ##    x = bd.nomes_alunos()
-## carregamento de nomes dos instrumentos
-
+#### funções para salvar no banco
+def salvar_aula():
+    tela_aula.combo_instrumentos.addItems(bd.nomes_instrumentos()) 
+    tela_aula.dateEdit.setMinimumDate(QDate.currentDate()) 
+    tela_aula.combo_alunos.addItems(bd.nomes_alunos())
+    instrumento = tela_aula.combo_instrumentos.currentText()
+    data = tela_aula.dateEdit.text()
+    aluno = tela_aula.combo_alunos.currentText()
+    conteudo = tela_aula.textEditConteudo.toPlainText()
+    print('Aluno: '+aluno+'Instrumento: '+
+          instrumento+'Data: '+data+'Conteúdo: '+conteudo)
+    if (instrumento == ''or data==''or instrumento==''or
+        conteudo==''):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Falta dados suficientes para salvar aula")
+        msgBox.setWindowTitle("Erro!")
+        msgBox.exec()
+    else:
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText('Dados Salvos!\nAluno: '+aluno+'\nInstrumento: '+
+        instrumento+'\nData: '+data+'\nConteúdo: '+conteudo)
+        msgBox.setWindowTitle("Registrando Aula")
+        msgBox.exec()
+        
+        tela_aula.close()
+    
+def salvar_aluno():
+    x = tela_registro_aluno.text_box_aluno.text()
+    d = datetime.now().strftime("%d/%m/%Y")
+    
+    if (tela_registro_aluno.combo_instrumento == '' or
+        x == ''):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Falta dados para salvar aluno")
+        msgBox.setWindowTitle("Erro!")
+        msgBox.exec()
+    else:
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Salvar "+x)
+        msgBox.setWindowTitle("salvar")
+        msgBox.exec()
+        bd.cadastrar_aluno(x, d)
+        tela_registro_aluno.close()
+        
+##def salvar_aula:
+def salvar_instrumento():
+    x = tela_instrumento.textbox.text()
+    if (x == ''):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Não foi informado o nome do instrumento")
+        msgBox.setWindowTitle("Erro Econtrado")
+        msgBox.exec()
+    else:
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Salvo com sucesso: "+x)
+        msgBox.setWindowTitle("Instrumento Cadastrado")
+        msgBox.exec()
+        bd.cadastrar_instrumento(x)
+        tela_instrumento.close()
+#### funções para excluir do banco
+##def excluir_aluno:
+##def excluir_aula:
+##def excluir_instrumento:
 def abrir_registro_aula():
+    registro_aula = bd.buscar_registro_aulas()
+    tela_aula.tableWidget.setRowCount(len(registro_aula))
+    print(len(registro_aula))
+    print(registro_aula[0][0])
+    row = 0
+    for x in registro_aula:
+        tela_aula.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(str(x[0])))
+        tela_aula.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(str(x[1])))
+        tela_aula.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(str(x[2])))
+        tela_aula.tableWidget.setItem(row, 3, QtWidgets.QTableWidgetItem(str(x[3])))
+        row=row+1
+
+    tela_aula.combo_instrumentos.addItems(bd.nomes_instrumentos()) 
     tela_aula.dateEdit.setMinimumDate(QDate.currentDate()) 
     tela_aula.combo_alunos.addItems(bd.nomes_alunos())
     tela_aula.show()
@@ -17,23 +98,30 @@ def abrir_tela_instrumentos():
     print(instrumento)
 def abrir_tela_registro_aluno():
     tela_registro_aluno.dateEdit.setMinimumDate(QDate.currentDate()) 
-    tela_registro_aluno.combo_instrumento.addItems(y)
+    tela_registro_aluno.combo_instrumento.addItems(bd.nomes_instrumentos())
     tela_registro_aluno.show()
 
-
 app = QtWidgets.QApplication(sys.argv)
-
+#tela menu
 tela_menu = uic.loadUi('tela_menu.ui')
-#tela instrumentos
-tela_instrumento = uic.loadUi('tela_instrumento.ui')
-tela_aula = uic.loadUi('tela_aula.ui')
-tela_registro_aluno = uic.loadUi('tela_registro_aluno.ui')
-y = bd.nomes_instrumentos()
-tela_aula.combo_instrumentos.addItems(y)
 tela_menu.btn_registrar_aula.clicked.connect(abrir_registro_aula)
 tela_menu.btn_instrumentos.clicked.connect(abrir_tela_instrumentos)
 tela_menu.btn_registrar_aluno.clicked.connect(abrir_tela_registro_aluno)
+#tela instrumentos
+tela_instrumento = uic.loadUi('tela_instrumento.ui')
+##y = bd.nomes_instrumentos()
+##z = bd.nomes_alunos()
+tela_instrumento.btn_salvar.clicked.connect(salvar_instrumento)
+#tela aula
+tela_aula = uic.loadUi('tela_aula.ui')
+
+tela_aula.btn_salvar.clicked.connect(salvar_aula)
+
+#tela registro aluno
+tela_registro_aluno = uic.loadUi('tela_registro_aluno.ui')
+tela_registro_aluno.btn_salvar.clicked.connect(salvar_aluno)
 #tela_menu.btn_visualizar_registros.clicked.connect(abrir_registros)
 bd.conn.close
 tela_menu.show()
 app.exec_()
+
